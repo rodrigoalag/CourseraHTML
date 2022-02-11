51bc6e3e951ca4def6a0748a1e8fdb11fcfdd707
Rodrigo <rodriaguilar1627@gmail.com>
2022-02-11 14:41:53 -0600

    asdada

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 (function() {
-    var names = ["Graciela", "Michelle", "Carolina", "Andrea", "Rodrigo", "Karla", "Keila", "Paula", "Diana", "Hillary"];
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
-        if (firstLetter === "k") {
+        if (firstLetter === "j") {
             byeSpeaker.speak(names[i]);
         } else {
             helloSpeaker.speak(names[i]);
